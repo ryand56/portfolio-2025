6a9dd4ef74bf122ca3bcc7f58f4cac22ccd96496
Ryan Omasta <git@ryand.ca>
2026-02-10 11:26:31 -0700

    Update for CF

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  reactStrictMode: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: ['skillicons.dev'],
+  },
 };
 
 export default nextConfig;
